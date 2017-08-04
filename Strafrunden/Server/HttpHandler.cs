0239cbc904c8e1d1c5ffe7c7b01396004842b7a4
TheIceman2706 <mayfriedrich@gmail.com>
2017-08-04 22:06:34 +0200

    building a http server...

---
+++ Strafrunden/Server/HttpHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Net = System.Net;
+using Http = System.Net.Http;
+
+namespace Strafrunden.Server
+{
+    class HttpHandler
+    {
+        public void GetContextCallback(IAsyncResult ar)
+        {
+            if (ar == null)
+                return;
+            Net.HttpListener listener = ar.AsyncState as Net.HttpListener;
+            Net.HttpListenerContext context = listener.EndGetContext(ar);
+
+            if (context.Request.HttpMethod == "GET" && context.Request.Url.LocalPath =="/strafrunden")
+            {
+                context.Response.StatusCode = 200;
+                string htmlTemplate = "<html><body>OK</body></html>";
+                byte[] buf = new byte[htmlTemplate.Length];
+
+                for(int i = 0; i < htmlTemplate.Length; i++)
+                {
+                    buf[i] = System.Convert.ToByte(htmlTemplate[i]);
+                }
+                context.Response.OutputStream.Write(buf, 0, buf.Length);
+            }
+        }
+    }
+}
---
+++ Strafrunden/Server/HttpServer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+
+namespace Strafrunden.Server
+{
+    public class HttpServer
+    {
+
+        private HttpListener _baseServer;
+        private HttpHandler _handler;
+        public HttpListener BaseServer { get => _baseServer; }
+
+        public HttpServer()
+        {
+            _handler = new HttpHandler();
+            _baseServer = new System.Net.HttpListener();
+
+            _baseServer.AuthenticationSchemes = AuthenticationSchemes.Anonymous;
+
+            _baseServer.Prefixes.Add("http://+:80/strafrunden/");
+
+            
+        }
+
+        public void Start() //TODO: make pulling context into loop (!)
+        {
+            _baseServer.Start();
+            _baseServer.BeginGetContext(_handler.GetContextCallback,_baseServer);
+        }
+
+        public void Stop()
+        {
+            _baseServer.Stop();
+        }
+    }
+}
