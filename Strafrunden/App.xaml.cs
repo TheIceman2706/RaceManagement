cd87cd03fb73dc51ee7bb1818f5ba266db600034
TheIceman2706 <mayfriedrich@gmail.com>
2017-11-22 20:12:09 +0100

    Update: Creating windows firewall rules via netsh

--- Strafrunden/App.xaml.cs
+++ Strafrunden/App.xaml.cs
@@ -64,6 +64,109 @@ namespace Strafrunden
                     }
                 }
             }
+
+            using (Process p = new Process())
+            {
+                p.StartInfo.FileName = "netsh.exe";
+                p.StartInfo.Arguments = "advfirewall firewall show rule name=\"Strafrundenserver\"";
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.Start();
+
+                output = p.StandardOutput.ReadToEnd();
+
+            }
+            if (!output.Contains("Strafrundenserver"))
+            {
+                using (Process p = new Process())
+                {
+                    p.StartInfo.FileName = "netsh.exe";
+                    p.StartInfo.Arguments = "advfirewall firewall add rule dir=in protocol=tcp action=allow localport=80  name=\"Strafrundenserver\"";
+                    p.StartInfo.UseShellExecute = true;
+                    p.StartInfo.Verb = "runas";
+                    p.StartInfo.RedirectStandardOutput = false;
+                    try
+                    {
+                        p.Start();
+                    }
+                    catch (Win32Exception ex)
+                    {
+                        if (ex.NativeErrorCode == 1223)
+                        {
+                            MessageBox.Show("Für die korrekte Funktion des Servers müssen Ports freigegeben werden. Dies benötigt Administratoren-Rechte.", "Fehler", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+                        else
+                        {
+                            throw;
+                        }
+                    }
+                }
+                using (Process p = new Process())
+                {
+                    p.StartInfo.FileName = "netsh.exe";
+                    p.StartInfo.Arguments = "advfirewall firewall add rule dir=out protocol=tcp action=allow localport=80  name=\"Strafrundenserver\"";
+                    p.StartInfo.UseShellExecute = true;
+                    p.StartInfo.Verb = "runas";
+                    p.StartInfo.RedirectStandardOutput = false;
+                    try
+                    {
+                        p.Start();
+                    }
+                    catch (Win32Exception ex)
+                    {
+                        if (ex.NativeErrorCode == 1223)
+                        {
+                            MessageBox.Show("Für die korrekte Funktion des Servers müssen Ports freigegeben werden. Dies benötigt Administratoren-Rechte.", "Fehler", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+                        else
+                        {
+                            throw;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            string output = "";
+            using (Process p = new Process())
+            {
+                p.StartInfo.FileName = "netsh.exe";
+                p.StartInfo.Arguments = "advfirewall firewall show rule name=\"Strafrundenserver\"";
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.Start();
+
+                output = p.StandardOutput.ReadToEnd();
+
+            }
+            if (output.Contains("Strafrundenserver"))
+            {
+                using (Process p = new Process())
+                {
+                    p.StartInfo.FileName = "netsh.exe";
+                    p.StartInfo.Arguments = "advfirewall firewall delete rule name=\"Strafrundenserver\"";
+                    p.StartInfo.UseShellExecute = true;
+                    p.StartInfo.Verb = "runas";
+                    p.StartInfo.RedirectStandardOutput = false;
+                    try
+                    {
+                        p.Start();
+                    }
+                    catch (Win32Exception ex)
+                    {
+                        if (ex.NativeErrorCode == 1223)
+                        {
+                            MessageBox.Show("Für die korrekte Funktion des Servers müssen Ports freigegeben werden. Dies benötigt Administratoren-Rechte.", "Fehler", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+                        else
+                        {
+                            throw;
+                        }
+                    }
+                }
+            }
         }
     }
 }
